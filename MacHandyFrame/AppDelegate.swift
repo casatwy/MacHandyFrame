52c99a8b709d707261b8359b79a58bb38f4ed468
casa <casa>
2020-08-06 13:53:24 +0800

    Initial Commit

---
+++ MacHandyFrame/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MacHandyFrame
+//
+//  Created by casa on 2020/8/6.
+//  Copyright © 2020 casa. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
